892cadba1376e775c8a65b1192d7e71feae64f00
a-ndrewlo <andrewlo6167@gmail.com>
2024-11-18 20:31:24 -0500

    finalized both position recognition and position updating

--- whackamole.py
+++ whackamole.py
@@ -1,3 +1,4 @@
+import random
 import pygame
 
 def main():
@@ -9,21 +10,28 @@ def main():
         screen = pygame.display.set_mode((640, 512))
         clock = pygame.time.Clock()
         running = True
+        mole_location = (0,0)
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     print(event.pos)
+                    if mole_location[0] <= event.pos[0] <= mole_location[0]+32 and mole_location[1] <= event.pos[1] <= mole_location[1]+32:
+                        print("hit")
+                        x_coord = random.randrange(0,20)*32
+                        y_coord = random.randrange(0,16)*32
+                        mole_location = (x_coord, y_coord)
+                        screen.blit(mole_image, mole_image.get_rect(topleft=(mole_location)))
             screen.fill("light green")
-            screen.blit(mole_image, mole_image.get_rect(topleft=(32,32)))
+            screen.blit(mole_image, mole_image.get_rect(topleft=(mole_location)))
             z=1
-            while z!=20:
+            for z in range(1,20):
                 x=32*z
                 pygame.draw.line(screen, color="black",start_pos=(x,0),end_pos=(x,512))
                 z+=1
             y=1
-            while y != 16:
+            for y in range (1,16):
                 x=32*y
                 pygame.draw.line(screen, color="black",start_pos=(0,x),end_pos=(640,x))
                 y+=1
