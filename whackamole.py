aefe64f57029bbafb0636919ac4660bfe1132357
a-ndrewlo <andrewlo6167@gmail.com>
2024-11-18 20:01:42 -0500

    added lines on the grid

--- whackamole.py
+++ whackamole.py
@@ -1,6 +1,5 @@
 import pygame
 
-#trying git add
 def main():
     try:
         pygame.init()
@@ -14,7 +13,20 @@ def main():
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    print(event.pos)
             screen.fill("light green")
+            screen.blit(mole_image, mole_image.get_rect(topleft=(32,32)))
+            z=1
+            while z!=20:
+                x=32*z
+                pygame.draw.line(screen, color="black",start_pos=(x,0),end_pos=(x,512))
+                z+=1
+            y=1
+            while y != 16:
+                x=32*y
+                pygame.draw.line(screen, color="black",start_pos=(0,x),end_pos=(640,x))
+                y+=1
             pygame.display.flip()
             clock.tick(60)
     finally:
